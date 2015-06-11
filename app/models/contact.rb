b63187c450c20d2d2572ebbeb52466463e4f2a92
benrmatthews <ben@montfort.io>
2015-06-11 20:01:14 +0000

    Changed contact show page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User'
   belongs_to :user
   has_and_belongs_to_many :lists
-  
+ 
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,8 +1,5 @@
 class List < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User'
-  has_and_belongs_to_many :contacts
-  
-  def name
-    "#{name.first}"
-  end
+  has_and_belongs_to_many :lists
+ 
 end
