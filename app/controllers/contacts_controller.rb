9bca8d0a1aa1f4bf3cd73d03966fd701c2ec8bd0
benrmatthews <ben@montfort.io>
2015-06-17 17:04:12 +0000

    Chanegd home page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -70,6 +70,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:first_name, :last_name, :title, :publication, :email, :twitter, :verified)
+      params.require(:contact).permit(:first_name, :last_name, :title, :publication, :email, :twitter, :verified, :list_id)
     end
 end
