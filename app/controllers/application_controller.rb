3ad5188d5db0bd2714487351dc4699b407ce4de4
benrmatthews <ben@montfort.io>
2015-06-23 12:06:59 +0000

    Amended home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case current_user.role
       when 'admin'
-        users_path
+        contacts_path
       when 'silver'
         contacts_path
       when 'gold'
