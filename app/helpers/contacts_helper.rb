024e8c31d06a3e94cf307b44085d6580f31f145f
benrmatthews <ben@montfort.io>
2015-07-16 15:12:32 +0000

    Updated gemfile

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -3,4 +3,10 @@ module ContactsHelper
     links = lists.collect{|list| link_to lists.name, lists_path(list)}
     return links.join(", ").html_safe
   end
+  
+  def index
+    @q = Contact.ransack(params[:q])
+    @contacts = @q.result(distinct: true)
+  end
+  
 end
