19457e1837b0a076a293a1dfd61de7efaad53e08
benrmatthews <ben@montfort.io>
2015-11-20 20:21:04 +0000

    Precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module Reachscout
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Automatically serve assets.
+    config.serve_static_assets = true
   end
 end
